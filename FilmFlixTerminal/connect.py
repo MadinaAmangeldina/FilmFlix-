86ab7c6623d2ee764d575525235e88a2fe8a2681
MadinaAmangeldina <161343110+MadinaAmangeldina@users.noreply.github.com>
2024-05-15 09:53:45 +0100

    moved terminal project to separate repository

--- FilmFlixTerminal/connect.py
+++
@@ -1,8 +0,0 @@
-import sqlite3 as sql
-
-try:
-  with sql.connect("C:/Users/madin/Desktop/JustIT Bootcamp/PythonProject/FilmFlixTerminal/filmflix.db") as conn:
-    cursor = conn.cursor()
-
-except sql.OperationalError as e:
-  print(f"Connection Failed!, {e}")
